2a3ba1d1c228e5b5fd691950ce708ca4d712dff5
shengyu98 <shengyu98@hotmail.com>
2023-09-30 23:04:30 +0800

    timetable

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { RegistrationComponent } from './registration/registration.component';
 import { SearchUserComponent } from './search-user/search-user.component';
 import { LoginComponent } from './login/login.component';
+import { TimetableComponent } from './timetable/timetable.component';
 
 const routes: Routes = [
   {
     path: "", component: LoginComponent, pathMatch: "full"},
       { path: "login", component: LoginComponent },
       { path: "register", component: RegistrationComponent },
+      { path: "timetable", component: TimetableComponent },
       { path: "search-user", component: SearchUserComponent }
 ];
 
